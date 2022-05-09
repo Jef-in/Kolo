cb151dbd9e58c89a95525e0eec6866cf67b57493
Jef-in <jefinabduljaleel@gmail.com>
2022-05-10 00:13:59 +0530

    Tab bar and individual controllers created

---
+++ KoloApp/KoloApp/Controllers/CharactersTabViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CharactersTabViewController.swift
+//  KoloApp
+//
+//  Created by Jefin on 10/05/22.
+//
+
+import UIKit
+
+class CharactersTabViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ KoloApp/KoloApp/Controllers/ComicsTabViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ComicsTabViewController.swift
+//  KoloApp
+//
+//  Created by Jefin on 10/05/22.
+//
+
+import UIKit
+
+class ComicsTabViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
