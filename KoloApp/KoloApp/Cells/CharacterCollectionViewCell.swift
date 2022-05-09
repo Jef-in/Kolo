4b436565c9830353c368fd66c5402fe7c2bf1b6a
Jef-in <jefinabduljaleel@gmail.com>
2022-05-10 01:05:27 +0530

    basic search bar and collectionview created for characters

---
+++ KoloApp/KoloApp/Cells/CharacterCollectionViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  CharacterCollectionViewCell.swift
+//  KoloApp
+//
+//  Created by Jefin on 10/05/22.
+//
+
+import Foundation
--- KoloApp/KoloApp/Controllers/CharactersTabViewController.swift
+++ KoloApp/KoloApp/Controllers/CharactersTabViewController.swift
@@ -8,8 +8,30 @@
 import UIKit
 
 class CharactersTabViewController: UIViewController {
+   
+    @IBOutlet weak var charactersCollectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        DispatchQueue.main.async {
+            self.charactersCollectionView.reloadData()
+        }
+    }
+}
+
+extension CharactersTabViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CharacterCollectionViewCell", for: indexPath) as! CharacterCollectionViewCell
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: (collectionView.frame.width / 2) - 10, height: 200)
     }
 }
